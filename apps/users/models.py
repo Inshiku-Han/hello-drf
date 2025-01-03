1632353b121c805156e34a2886f95ec35d3f0307
Insik Han Windows <gksdudrms0@gmail.com>
2025-01-03 13:34:26 +0900

    feat: Add relation between User and Student

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,10 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+from apps.students.models import Student
+
 
 class CustomUser(AbstractUser):
     phone_number = models.CharField(
         max_length=15,
         blank=True,
         null=True,
+    )
+    student = models.OneToOneField(
+        Student, on_delete=models.CASCADE, null=True, blank=True, related_name="user"
     )
